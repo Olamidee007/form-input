536d5023fce1d237bd49bb85c3cfdde50bdc79db
Olamidee007 <lamide.dammy@gmail.com>
2022-03-25 16:41:55 +0100

    create new file

---
+++ INTRO-JS/script.js
@@ -0,0 +1,18 @@
+let user_time=30
+const userscore=100
+"const UserName = document.getElementById("UserTitle") 
+        UserName.addEventListener("click",{}=>{prompt ("Welcome to the page")})"
+
+        let user_time2 = "30"
+
+        let greetings = "Welcome to my site"
+
+        let my
+
+        // Datatype
+        // 1. Number
+        // 2. Strings
+        // 3. Boolean
+        // 4. Symbols
+        // 5. Null
+        // 6. Undefined
